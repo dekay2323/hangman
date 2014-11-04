45b8c907dcf4cf3b4e330a5bae39c3eb3807b8ad
DemianK <demiankrige@gmail.com>
2014-11-04 16:05:35 -0700

    Getting the basic Admin,User role in

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import com.hangman.Role
+import com.hangman.User
+import com.hangman.UserRole
+
 class BootStrap {
 
+
     def init = { servletContext ->
-    }
+    	def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+     	def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+     	def adminUser = new User(username: 'admin', password:'admin', enabled: true)
+     	adminUser.save(flush: true)
+
+     	UserRole.create adminUser, adminRole, true
+
+     	assert User.count() == 1
+     	assert Role.count() == 2
+     	assert UserRole.count() == 1
+	}
+
     def destroy = {
     }
 }
